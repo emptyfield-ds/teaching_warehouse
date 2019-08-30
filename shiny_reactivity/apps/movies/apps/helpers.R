99843bbde73b2ad87b87766da5bcf48084e83662
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-08-29 21:59:55 -0700

    clean up materials and add second half

---
+++ shiny_reactivity/apps/movies/apps/helpers.R
@@ -0,0 +1,8 @@
+# Function for prettifying axis labels
+prettify_label <- function(x){
+  x %>%
+    str_replace_all("_", " ") %>%
+    str_to_title() %>%
+    str_replace("Imdb", "IMDB") %>%
+    str_replace("Mpaa", "MPAA")
+}
---
+++ shiny_reactivity/apps/movies/apps/movies_07.R
@@ -0,0 +1,97 @@
+# Load packages -----------------------------------------------------
+library(shiny)
+library(tidyverse)
+library(DT)
+library(tools)
+
+# Load data ---------------------------------------------------------
+load("data/movies.Rdata")
+
+# Load helpers ------------------------------------------------------
+source("helpers.R")
+
+# Define UI for application that plots features of movies -----------
+ui <- fluidPage(
+  
+  # Application title -----------------------------------------------
+  titlePanel("Movie browser"),
+  
+  # Sidebar layout with a input and output definitions --------------
+  sidebarLayout(
+    
+    # Inputs: Select variables to plot ------------------------------
+    sidebarPanel(
+      
+      # Select variable for y-axis ----------------------------------
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("IMDB rating" = "imdb_rating", 
+                              "IMDB number of votes" = "imdb_num_votes", 
+                              "Critics Score" = "critics_score", 
+                              "Audience Score" = "audience_score", 
+                              "Runtime" = "runtime"), 
+                  selected = "audience_score"),
+      
+      # Select variable for x-axis ----------------------------------
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = c("IMDB rating" = "imdb_rating", 
+                              "IMDB number of votes" = "imdb_num_votes", 
+                              "Critics Score" = "critics_score", 
+                              "Audience Score" = "audience_score", 
+                              "Runtime" = "runtime"), 
+                  selected = "critics_score"),
+      
+      # Select variable for color -----------------------------------
+      selectInput(inputId = "z", 
+                  label = "Color by:",
+                  choices = c("Title Type" = "title_type", 
+                              "Genre" = "genre", 
+                              "MPAA Rating" = "mpaa_rating", 
+                              "Critics Rating" = "critics_rating", 
+                              "Audience Rating" = "audience_rating"),
+                  selected = "mpaa_rating"),
+      
+      # Set alpha level ---------------------------------------------
+      sliderInput(inputId = "alpha", 
+                  label = "Alpha:", 
+                  min = 0, max = 1, 
+                  value = 0.5)
+      
+    ),
+    
+    # Output --------------------------------------------------------
+    mainPanel(
+
+      # Show scatterplot --------------------------------------------
+      plotOutput(outputId = "scatterplot"),
+
+      # Show data table ---------------------------------------------
+      DT::dataTableOutput(outputId = "moviestable")
+    )
+  )
+)
+
+# Define server function required to create the scatterplot ---------
+server <- function(input, output) {
+  
+  # Create scatterplot object the plotOutput function is expecting --
+  output$scatterplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x, y = input$y,
+                                     color = input$z)) +
+      geom_point(alpha = input$alpha) +
+      labs(x = prettify_label(input$x),
+           y = prettify_label(input$y),
+           color = prettify_label(input$z))
+  })
+  
+  # Print data table if checked -------------------------------------
+  output$moviestable <- DT::renderDataTable({
+    DT::datatable(data = movies[, 1:7], 
+                  options = list(pageLength = 10), 
+                  rownames = FALSE) 
+  })
+}
+
+# Create the Shiny app object ---------------------------------------
+shinyApp(ui = ui, server = server)
