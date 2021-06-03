a86afed7ccc0e6a2c51b0f25cc47dc4aa547ad92
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-06-03 11:55:27 -0700

    add YT3 template

---
+++ targets_whole_game/templates/your_turn_3.R
@@ -0,0 +1,15 @@
+library(targets)
+library(tarchetypes)
+options(tidyverse.quiet = TRUE)
+
+# load packages prior to our pipeline
+# this is the same as including a script that calls `library()` for each package
+tar_option_set(packages = c("tidyverse", "maps", "sf", "scico"))
+source("R/functions.R")
+
+# end your _targets.R file with a list of targets
+tar_plan(
+  tar_file(forest_file, "data/forest.csv"),
+  forest = read_csv(forest_file),
+  forest_map = map_2010_forest_conversion(forest)
+)
