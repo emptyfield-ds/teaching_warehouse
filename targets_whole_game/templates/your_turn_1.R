9253132f8017c235df60466b9f5204ad7f7fa966
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-06-03 11:51:09 -0700

    update `copy_targets_template()` and add usethis to `.deps`

---
+++ targets_whole_game/templates/your_turn_1.R
@@ -0,0 +1,12 @@
+library(targets)
+library(tarchetypes)
+options(tidyverse.quiet = TRUE)
+
+# load packages prior to our pipeline
+# this is the same as including a script that calls `library()` for each package
+tar_option_set(packages = c())
+
+# end your _targets.R file with a list of targets
+tar_plan(
+
+)
