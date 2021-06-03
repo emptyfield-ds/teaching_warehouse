86756067c0bdfbff4ed2b2e5058c76797398196c
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-06-03 15:58:04 -0700

    clean up and re-render slides

--- targets_basics/slides_pipeline/R/slide_functions.R
+++ targets_basics/slides_pipeline/R/slide_functions.R
@@ -1,7 +1,12 @@
 create_line_plot <- function(gapminder) {
   gapminder %>%
     filter(continent != "Oceania2121") %>%
-    ggplot(aes(x = year, y = lifeExp, group = country, color = country)) +
+    ggplot(aes(
+      x = year,
+      y = lifeExp,
+      group = country,
+      color = country
+    )) +
     geom_line(lwd = 1, show.legend = FALSE) +
     facet_wrap(~ continent) +
     scale_color_manual(values = country_colors) +
