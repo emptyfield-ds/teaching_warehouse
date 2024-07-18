4d186a24458c20b21b8b2fd2bb648ad8ab3a7f3a
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-07-18 13:24:01 -0400

    add script to sync mastering r modules

---
+++ R/sync_mastering_r.R
@@ -0,0 +1,22 @@
+modules <- c(
+  "intro",
+  "reading_data",
+  "dplyr_5verbs",
+  "ggplot_basics",
+  "ggplot_customizing",
+  "ggplot_recreate_challenge",
+  "tidyr_basics",
+  "modeling_and_broom",
+  "types_and_functions",
+  "purrr_basics",
+  "quarto_basics",
+  "quarto_whole_game",
+  # because I usually touch these too when updating the above
+  "py_quarto_basics",
+  "ds_whole_game",
+  "py_quarto_whole_game",
+  "tidy_tuesday"
+)
+
+
+purrr::walk(modules, shunyata::sync_module)
